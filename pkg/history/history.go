7a32fcfd1a94532179dfe4d228dd28d6995f6c95
xiaobing <xiaobingpang@qq.com>
2018-03-08 23:07:15 +0800

    1 getCNStockKData daily period implement

--- pkg/history/history.go
+++ pkg/history/history.go
@@ -1,6 +1,18 @@
 package history
 
-import "github.com/mineralres/goshare/aproto"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/mineralres/goshare/pkg/base"
+
+	"github.com/mineralres/goshare/aproto"
+)
 
 // GetKData 请求历史K线数据
 /*
@@ -11,6 +23,71 @@ period：周期
 retryCount：当网络异常后重试次数，默认为3
 */
 func GetKData(symbol *aproto.Symbol, period aproto.PeriodType, startDate, endDate, retryCount int) (*aproto.KlineSeries, error) {
+	if symbol.Exchange == aproto.ExchangeType_SSE || symbol.Exchange == aproto.ExchangeType_SZE {
+		return getCNStockKData(symbol, period, startDate, endDate, retryCount)
+	}
 	var ret aproto.KlineSeries
+	return &ret, nil
+}
+
+func getCNStockKData(symbol *aproto.Symbol, period aproto.PeriodType, startDate, endDate, retryCount int) (*aproto.KlineSeries, error) {
+	var ret aproto.KlineSeries
+	et := 1
+	if symbol.Exchange == aproto.ExchangeType_SZE {
+		et = 2
+	}
+	address := fmt.Sprintf("http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?rtntype=5&id=%s%d&type=k", symbol.Code, et)
+	resp, err := http.Get(address)
+	if err != nil {
+		return &ret, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &ret, err
+	}
+	data := string(body)
+	data = strings.TrimLeft(data, "(")
+	data = strings.TrimRight(data, ")")
+	var rtn struct {
+		Name string `json:"name"`
+		Code string `json:"code"`
+		Info struct {
+			C string `json:"c"`
+		} `json:"info"`
+		Data []string `json:"data"`
+	}
+	err = json.Unmarshal([]byte(data), &rtn)
+	if err != nil {
+		return &ret, err
+	}
+	for i := range rtn.Data {
+		items := strings.Split(rtn.Data[i], ",")
+		if len(items) == 8 {
+			var k aproto.Kline
+			tm, err := time.Parse("2006-01-02", items[0])
+			if err != nil {
+				log.Println(err, items[0])
+				continue
+			}
+			k.Time = tm.Unix()
+			k.Open = base.ParseFloat(items[1])
+			k.Close = base.ParseFloat(items[2])
+			k.High = base.ParseFloat(items[3])
+			k.Low = base.ParseFloat(items[4])
+			k.Volume = base.ParseFloat(items[5])
+			if strings.Contains(items[6], "万") {
+				val := strings.Replace(items[6], "万", "", -1)
+				k.Amount = base.ParseFloat(val) * 10000
+			} else if strings.Contains(items[6], "亿") {
+				val := strings.Replace(items[6], "亿", "", -1)
+				k.Amount = base.ParseFloat(val) * 100000000
+			} else {
+				k.Amount = base.ParseFloat(items[6])
+			}
+			ret.List = append(ret.List, k)
+		}
+	}
+	log.Println(ret, err)
 	return &ret, nil
 }
--- pkg/history/history_test.go
+++ pkg/history/history_test.go
@@ -2,12 +2,17 @@ package history
 
 import (
 	"log"
+	"testing"
 
 	"github.com/mineralres/goshare/aproto"
 )
 
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
 // TestGetKData TestGetKData
-func TestGetKData() {
+func TestGetKData(t *testing.T) {
 	symbol := aproto.Symbol{Exchange: aproto.ExchangeType_SSE, Code: "600000"}
 	log.Println(GetKData(&symbol, aproto.PeriodType_D1, 19990101, 20180307, 1))
 }
