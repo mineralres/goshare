755a1cd4384cd1d39ebe1e096c9d94341963feb0
jinlong <pajl222@126.com>
2018-03-11 20:29:54 +0800

    :jinlong add tick sina

--- pkg/realtime/realtime_test.go
+++ pkg/realtime/realtime_test.go
@@ -3,7 +3,7 @@ package realtime
 import (
 	"log"
 	"testing"
-
+	
 	"github.com/mineralres/goshare/aproto"
 )
 
@@ -13,7 +13,7 @@ func init() {
 
 // TestGetLastTick TestGetLastTick
 func TestGetLastTick(t *testing.T) {
-	symbol := aproto.Symbol{Exchange: aproto.ExchangeType_SSE, Code: "600000"}
+	symbol := aproto.Symbol{Exchange: aproto.ExchangeType_SSE, Code: "600000"}	
 	md, err := GetLastTick(&symbol)
 	if err != nil {
 		t.Error(err)
@@ -23,3 +23,35 @@ func TestGetLastTick(t *testing.T) {
 	}
 	log.Printf("Tick[%s], Open[%.2f], High[%.2f], Low[%.2f], Close[%.2f]", md.Symbol.Code, md.Open, md.High, md.Low, md.Close)
 }
+
+func TestIndexTick(t *testing.T) {
+	//测试获取sina各种指数
+	log.Printf("测试获取sina各种指数")
+
+	m_index := map[string]string{
+		"道琼斯指数" : "int_dji",
+		"上证指数" : "sh000001",
+		"纳斯达克" : "int_nasdaq",
+		"恒生指数" : "int_hangseng",
+		"日经指数" : "b_TWSE",
+		"新加坡指数" : "b_FSSTI",
+	}	
+
+	
+	for key, views := range m_index {		
+		symbol := aproto.Symbol{Exchange: aproto.ExchangeType_INDEX, Code: views}
+		md, err := GetLastTick(&symbol)
+		if err != nil {
+			t.Error(err)
+		}
+		if (md.Close) <= 0 {
+			t.Error("获取行情为空")
+		}
+		md.Symbol.Code = key
+		log.Printf("Tick[%s],Close[%.2f]", md.Symbol.Code,  md.Close)
+	}
+
+	
+
+}
+
