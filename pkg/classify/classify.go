248b4d05fc64c613ffbcd62b58c0e224565c4b1e
rucheng <rucheng@localhost.localdomain>
2018-03-19 17:19:12 +0800

    add get_index_member

---
+++ pkg/classify/classify.go
@@ -0,0 +1,57 @@
+package classify
+
+import (
+	// "log"
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/mineralres/goshare/aproto"
+)
+
+// ClassifyProvider Provider
+type ClassifyProvider struct {
+}
+
+func (p *ClassifyProvider) GetIndexMember(symbol *aproto.Symbol, retryCount int) ([]aproto.Symbol, error) {
+	return getIndexMem(symbol)
+	// var ret []aproto.Symbol;
+	// return ret, nil
+}
+
+func getIndexMem(symbol *aproto.Symbol) ([]aproto.Symbol, error) {
+	var ret []aproto.Symbol
+
+	page_number := 1
+	member_number := 0
+
+	for true{
+		address := fmt.Sprintf("http://vip.stock.finance.sina.com.cn/corp/view/vII_NewestComponent.php?page=%d&indexid=%s", page_number, symbol.Code)
+		// log.Println(address)
+		page_number++;
+		doc, err := goquery.NewDocument(address)
+
+		if err != nil {  
+			fmt.Println(err)
+			return ret, err
+		}
+
+		b_empty := true;
+		
+		doc.Find("#NewStockTable").Find("tbody").Find("tr").Each(func(i int, s *goquery.Selection) {  
+			if i > 0{
+				b_empty = false;
+				code := s.Find("div").Eq(0).Text()
+				s := aproto.Symbol{Exchange: aproto.ExchangeType_SSE, Code: code}
+				// log.Println(code)
+				ret = append(ret, s)
+				member_number += 1;
+			} 
+		});
+		if b_empty == true || doc.Find("#page_form").Length() == 0{
+			break;
+		}
+	}
+	
+	// log.Println(ret)
+	return ret, nil
+}
---
+++ pkg/classify/classify_test.go
@@ -0,0 +1,31 @@
+package classify
+
+import (
+	"log"
+	"testing"
+
+	"github.com/mineralres/goshare/aproto"
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+// TestIndexMemberData 
+func TestIndexMemberData(t *testing.T) {
+
+	// symbol := aproto.Symbol{Exchange: aproto.ExchangeType_SSE, Code: "000016"}
+	symbol := aproto.Symbol{Exchange: aproto.ExchangeType_SSE, Code: "000905"}
+	var p ClassifyProvider
+	arr_symbol, err := p.GetIndexMember(&symbol, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	i := 0
+	for i < len(arr_symbol){
+		log.Println(arr_symbol[i]);
+		i++;
+	}
+	log.Println("000300 member size =", len(arr_symbol));
+}
